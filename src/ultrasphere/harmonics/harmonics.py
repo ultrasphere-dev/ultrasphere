f3ded8feb4ecf89e399a5cc3a50209b16ef588a6
34j <34j.github@proton.me>
2025-08-20 20:55:55 +0900

    fix: do not use .values

--- src/ultrasphere/harmonics/harmonics.py
+++ src/ultrasphere/harmonics/harmonics.py
@@ -247,7 +247,8 @@ def concat_harmonics(
         if c.s_ndim == 0:
             return xp.asarray(1)
         return xp.prod(
-            xp.stack(xp.broadcast_arrays(*[harmonics[k] for k in c.s_nodes]), axis=0), axis=0
+            xp.stack(xp.broadcast_arrays(*[harmonics[k] for k in c.s_nodes]), axis=0),
+            axis=0,
         )
     except Exception as e:
         shapes = {k: v.shape for k, v in harmonics.items()}
--- tests/conftest.py
+++ tests/conftest.py
@@ -10,14 +10,19 @@ def xp(request: pytest.FixtureRequest) -> ArrayNamespaceFull:
     backend = request.param
     if backend == "numpy":
         import numpy as xp
+
         rng = xp.random.default_rng()
+
         def random_uniform(low=0, high=1, shape=None):
             return rng.random(shape) * (high - low) + low
+
         xp.random.random_uniform = random_uniform
     elif backend == "torch":
         import torch as xp
+
         def random_uniform(low=0, high=1, shape=None):
             return xp.rand(shape) * (high - low) + low
+
         xp.random.random_uniform = random_uniform
     else:
         raise ValueError(f"Unknown backend: {backend}")
