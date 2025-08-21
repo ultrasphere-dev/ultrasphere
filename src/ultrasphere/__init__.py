6d11d56889b32fb46f4f2214c77c8d40d3c8a472
34j <34j.github@proton.me>
2025-08-21 18:28:02 +0900

    refactor: export modules

--- src/ultrasphere/__init__.py
+++ src/ultrasphere/__init__.py
@@ -12,19 +12,37 @@ from ._creation import (
 from ._draw import draw
 from ._integral import integrate, roots
 from ._random import random_ball
+from .special import (
+    fundamental_solution,
+    lgamma,
+    potential_coef,
+    shn1,
+    shn2,
+    sjv,
+    syv,
+    szv,
+)
 
 __all__ = [
     "SphericalCoordinates",
     "c_spherical",
     "draw",
     "from_branching_types",
+    "fundamental_solution",
     "hopf",
     "integrate",
+    "lgamma",
     "polar",
+    "potential_coef",
     "random",
     "random",
     "random_ball",
     "roots",
+    "shn1",
+    "shn2",
+    "sjv",
     "standard",
     "standard_prime",
+    "syv",
+    "szv",
 ]
